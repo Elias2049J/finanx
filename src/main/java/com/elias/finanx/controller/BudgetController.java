57a04987760c2bb63f57c6b8f2d9bc6b626a1d64
Miguel Centellas <163223596+reyrojo2@users.noreply.github.com>
2026-06-13 15:29:47 -0500

    Se corrige "bugdets" por "budgets"

--- src/main/java/com/elias/finanx/controller/BudgetController.java
+++ src/main/java/com/elias/finanx/controller/BudgetController.java
@@ -13,7 +13,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/bugdets")
+@RequestMapping("/budgets")
 @RequiredArgsConstructor
 public class BudgetController {
     private final BudgetService budgetService;
